ec5487653475f153afe66562da99dd795acd0f25
Will Winder <wwinder.unh@gmail.com>
2025-07-09 08:56:50 -0400

    Copilot feedback.

--- safebigint.go
+++ safebigint.go
@@ -38,30 +38,32 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	return nil, nil
 }
 
-// truncatingMap holds methods to flag for the silent truncate or overflow warning.
-var truncatingMap = map[string]string{
-	"Uint64": "Uint64()",
-	"Int64":  "Int64()",
-}
-
 // checkForTruncation looks for unsafe calls that may result in truncation.
 func checkForTruncation(pass *analysis.Pass, sel *ast.SelectorExpr, call *ast.CallExpr) {
-	if truncMsg, exists := truncatingMap[sel.Sel.Name]; !exists {
+	// methods to flag for the silent truncate or overflow warning.
+	truncatingMap := map[string]string{
+		"Uint64": "Uint64()",
+		"Int64":  "Int64()",
+	}
+
+	var truncMsg string
+	var exists bool
+	if truncMsg, exists = truncatingMap[sel.Sel.Name]; !exists {
 		return
-	} else {
-		recv := pass.TypesInfo.TypeOf(sel.X)
-		ptrType, ok := recv.(*types.Pointer)
-		if !ok {
-			return
-		}
+	}
 
-		named, ok := ptrType.Elem().(*types.Named)
-		if !ok || named.Obj().Pkg() == nil {
-			return
-		}
+	recv := pass.TypesInfo.TypeOf(sel.X)
+	ptrType, ok := recv.(*types.Pointer)
+	if !ok {
+		return
+	}
 
-		if named.Obj().Pkg().Path() == "math/big" && named.Obj().Name() == "Int" {
-			pass.Reportf(call.Pos(), "calling %s on *big.Int may silently truncate or overflow", truncMsg)
-		}
+	named, ok := ptrType.Elem().(*types.Named)
+	if !ok || named.Obj().Pkg() == nil {
+		return
+	}
+
+	if named.Obj().Pkg().Path() == "math/big" && named.Obj().Name() == "Int" {
+		pass.Reportf(call.Pos(), "calling %s on *big.Int may silently truncate or overflow", truncMsg)
 	}
 }
